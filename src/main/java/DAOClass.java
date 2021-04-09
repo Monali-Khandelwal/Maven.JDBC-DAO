e900bcbdccee7a5db9a420a1bb0ba201b645ae3b
Monali <khandelwal.monali@gmail.com>
2021-04-09 17:59:03 -0400

    Completed the lab

--- src/main/java/DAOClass.java
+++ src/main/java/DAOClass.java
@@ -1,9 +1,8 @@
 import com.mysql.cj.jdbc.CallableStatement;
+import com.mysql.cj.jdbc.ConnectionImpl;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.List;
-import java.sql.Statement;
 
 public class DAOClass <T>implements DAO <T>{
 
@@ -18,7 +17,8 @@ public class DAOClass <T>implements DAO <T>{
         ResultSet rs = stm.executeQuery("select * from car where ID =" + id);
         //rs.first();
         while (rs.next()) {
-            System.out.println(rs.getInt("ID: " + id));
+            System.out.println(rs.getString("MAKE"));
+            System.out.println(rs.getString("MODEL"));
         }
         System.out.println();
         return null;
@@ -28,7 +28,7 @@ public class DAOClass <T>implements DAO <T>{
         ResultSet rs = stm.executeQuery("select * from car");
         //rs.first();
         while (rs.next()) {
-            System.out.println(rs.getInt("ID: "));
+            System.out.println(rs.getInt("ID"));
             System.out.println(rs.getString("MAKE"));
             System.out.println(rs.getString("MODEL"));
             System.out.println(rs.getInt("YEAR"));
@@ -40,15 +40,42 @@ public class DAOClass <T>implements DAO <T>{
     }
 
     public T update(T dto) {
+        Connection connection = Runner.getConnection();
+        try {
+            PreparedStatement pStmt = connection.prepareStatement("UPDATE car SET MODEL = 'Cherokee' WHERE ID = 102");
+            pStmt.executeUpdate();
 
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
         return null;
     }
 
     public T create(T dto) {
+        Connection connection = Runner.getConnection();
+        System.out.println("Creating new record");
+        try {
+            PreparedStatement pStmt = connection.prepareStatement("INSERT INTO car VALUES  ('107', 'BMW', 'X61', '2020', 'White', '1HGBH41JXNG876555')");
+//            pStmt.setInt(1, 106);
+//            pStmt.setString(2, "BMW");
+//            pStmt.setString(3, "X60");
+            pStmt.executeUpdate();
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
         return null;
     }
 
     public void delete(int id) {
+        Connection connection = Runner.getConnection();
+        try {
+            Statement stm = connection.createStatement();
+            int i = stm.executeUpdate("DELETE from car WHERE ID =" + id);
+
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
 
     }
 }
--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -7,51 +7,91 @@ public class Runner {
     static String username = "Monali";
     static String password = "Monali";
     static String dbUrl = "jdbc:mysql://localhost:3306/NewDB";
-    static Logger demoLog = Logger.getLogger("demoJDBC");
+    //static Logger demoLog = Logger.getLogger("demoJDBC");
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(dbUrl, username, password);
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database", e);
+        }
+    }
 
 
     public static void main(String[] args) {
-        // try catch block for Logger
-        demoLog.log(Level.INFO, "Car database");
-        System.out.println("-------- MySQL JDBC Connection Demo ------------");
+        Connection connection = Runner.getConnection();
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-        } catch (ClassNotFoundException e) {
-            System.out.println("MySQL JDBC Driver not found !!");
-            return;
-        }
-        System.out.println("MySQL JDBC Driver Registered!");
-
-
-        //Try catch finally block to develop connection
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(dbUrl, username, password);
             System.out.println("SQL Connection to database established!");
+            Statement stm = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
 
-            // The function goes here!!
-           Statement stm = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            DAOClass dao = new DAOClass(stm);
+            System.out.println("find by id");
+            dao.findById(101);
+            dao.findAll();
+            dao.update(102);
+            dao.delete(105);
+            dao.create(106);
 
-           DAOClass dao = new DAOClass (stm);
-           System.out.println("find by id");
-           dao.findById(101);
-           dao.findAll();
-//            System.out.println("delete");
 
-        }catch (SQLException e) {
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
             System.out.println("Connection Failed! Check output console");
             return;
         }
-        finally {
-            try
-            {
-                if(connection != null)
-                    connection.close();
-                System.out.println("Connection closed !!");
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+
     }
 }
+
+
+
+
+
+
+
+
+        // try catch block for Logger
+//        demoLog.log(Level.INFO, "Car database");
+//        System.out.println("-------- MySQL JDBC Connection Demo ------------");
+//        try {
+//            Class.forName("com.mysql.jdbc.Driver");
+//        } catch (ClassNotFoundException e) {
+//            System.out.println("MySQL JDBC Driver not found!!");
+//            return;
+//        }
+//        System.out.println("MySQL JDBC Driver Registered!");
+
+
+        //Try catch finally block to develop connection
+
+//        Connection connection = null;
+//        try {
+//            connection = DriverManager.getConnection(dbUrl, username, password);
+//            System.out.println("SQL Connection to database established!");
+//
+//            // The function goes here!!
+//           Statement stm = connection.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
+//
+//           DAOClass dao = new DAOClass (stm);
+//           System.out.println("find by id");
+//           dao.findById(101);
+//           dao.findAll();
+//           dao.update()
+////            System.out.println("delete");
+//
+//        }catch (SQLException e) {
+//            System.out.println(e.getMessage());
+//            System.out.println("Connection Failed! Check output console");
+//            return;
+//        }
+//        finally {
+//            try
+//            {
+//                if(connection != null)
+//                    connection.close();
+//                System.out.println("Connection closed!!");
+//            } catch (SQLException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+
